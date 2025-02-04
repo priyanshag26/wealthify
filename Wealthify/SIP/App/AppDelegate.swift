1689eb2de1bff9615c91fdcb5c3a28cde5117837
Priyansh Agarwal <187436469+priyanshag26@users.noreply.github.com>
2025-02-04 23:30:36 +0530

    Delete Wealthify directory

--- Wealthify/SIP/App/AppDelegate.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import SwiftUI
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        return true
-    }
-}
